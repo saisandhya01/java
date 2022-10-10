a467902d68f7492b1b4e5161b9deeff017668e04
Sai <saisandhya187@gmail.com>
2022-10-10 10:23:09 +0530

    Variables

--- section-3/Helloworld/src/hello.java
+++ section-3/Helloworld/src/hello.java
@@ -1,5 +1,15 @@
 public class hello {
     public static void main(String[] args){
         System.out.println("Hello world!!");
+        //string literal: anything inside double quotes
+
+        //Variables
+        int myFirstNumber = 35; //declaration statement
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+        //type sout and then tab - shortcut
     }
 }
