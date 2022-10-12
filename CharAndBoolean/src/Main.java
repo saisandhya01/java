7d2dd927f62d5776b656aac3fffefb267c168b5f
Sai <saisandhya187@gmail.com>
2022-10-12 11:07:28 +0530

    char and boolean

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'd'; //one byte in space
+        char myUnicodeChar = '\u0B9A'; //taken from unicode table, search in google
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+    }
+}
