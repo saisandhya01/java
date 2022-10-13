99b398160e9b25f006335da3f1a75fc3c9d98364
Sai <saisandhya187@gmail.com>
2022-10-13 10:11:33 +0530

    abbreviating operators

---
+++ Operators/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //abbreviating operators
+        int result = 1;
+        result++;
+        System.out.println(result);
+
+        result--;
+        System.out.println(result);
+
+        result += 2;
+        System.out.println(result);
+
+        result *= 10;
+        System.out.println(result);
+    }
+}
