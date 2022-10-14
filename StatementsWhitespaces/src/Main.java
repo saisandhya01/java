767e2d8a31a2a356f0878672e85397ef5e4be9bc
Sai <saisandhya187@gmail.com>
2022-10-14 10:13:11 +0530

    Statements, Whitespace and Indentation

---
+++ StatementsWhitespaces/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        //statement is the entire line
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test"); //method call
+        //the above line is a statement. Method calls are statements
+
+        int anotherVariable = 50; anotherVariable++; //two statements in one line
+        //Indentation
+    }
+}
