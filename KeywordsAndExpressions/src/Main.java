3f2671a4ce0b10524ffcc3f08bfe2fa24f58d32e
Sai <saisandhya187@gmail.com>
2022-10-14 10:01:15 +0530

    keywords and expressions

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //keywords
+        int int2 = 5;
+        //List of java keywords: https://en.wikipedia.org/wiki/List_of_Java_keywords
+        double kilometers = (100 * 1.609344); //expression
+        //kilometers = (100 * 1.609344) is expression
+        //if statements are expressions
+        //anything under double quotes are expression
+    }
+}
