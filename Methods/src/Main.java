51b644f74b8dc7bb4cd2054384f2024dd3d99925
Sai <saisandhya187@gmail.com>
2022-10-15 11:15:10 +0530

    Fix: when score is 1000 or 500

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -18,13 +18,13 @@ public class Main {
         displayHighScorePosition("Rak", calculateHighScorePosition(50));
     }
     public static int calculateHighScorePosition(int score){
-        if(score > 1000){
+        if(score >= 1000){
             return 1;
         }
-        else if(score > 500 && score < 1000){
+        else if(score >= 500 && score < 1000){
             return 2;
         }
-        else if(score > 100 && score < 500){
+        else if(score >= 100 && score < 500){
             return 3;
         }
         return 4;
