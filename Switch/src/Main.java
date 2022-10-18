910edd1f098d5f1d74854f3604528f2c85510a4f
Sai <saisandhya187@gmail.com>
2022-10-18 11:22:16 +0530

    switch statements

---
+++ Switch/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+       int switchValue = 5;
+       //break statement is important
+        //switch statements can be used with byte, char, int, short
+       switch (switchValue){
+           case 1:
+               System.out.println("Value was 1");
+               break;
+           case 2:
+               System.out.println("Value was 2");
+               break;
+           case 3: case 4: case 5:
+               System.out.println("value was 3 or 4 or 5");
+               break;
+           default:
+               System.out.println("Was not 1 or 2");
+               //break;
+       }
+
+    }
+}
