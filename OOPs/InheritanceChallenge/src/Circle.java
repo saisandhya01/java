edad22b22c0b87fc69886698d3483f5cd039e51a
Sai <saisandhya187@gmail.com>
2022-11-16 10:45:00 +0530

    inheritance coding exercise 1

---
+++ OOPs/InheritanceChallenge/src/Circle.java
@@ -0,0 +1,17 @@
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+        if(radius < 0){
+            this.radius = 0;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+}
---
+++ OOPs/InheritanceChallenge/src/Cylinder.java
@@ -0,0 +1,18 @@
+public class Cylinder extends Circle{
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = height;
+        if(height < 0){
+            this.height = 0;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume(){
+        return getArea() * height;
+    }
+}
